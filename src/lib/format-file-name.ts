1407479bc8fc23adb0da1b1f21d5b698be8b3194
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2020-11-07 22:37:31 +0100

    Continue renaming on error

--- src/lib/format-file-name.ts
+++ src/lib/format-file-name.ts
@@ -8,30 +8,34 @@ function zeroPad(value: number, maxLength: number = 2): string {
 export async function formatFileName(
   file: FileSystemFileHandle
 ): Promise<string> {
-  // Only read the first 128kB.
-  const size = 128 * 1024;
-  const data = await readFromFile(file, size);
-  const { load } = await import("exifreader");
-  const tags = load(data);
-  if (
-    tags !== undefined &&
-    tags.DateTimeOriginal !== undefined &&
-    tags.DateTimeOriginal.value !== undefined &&
-    tags.DateTimeOriginal.value[0] === undefined
-  ) {
+  try {
+    // Only read the first 128kB.
+    const size = 128 * 1024;
+    const data = await readFromFile(file, size);
+    const { load } = await import("exifreader");
+    const tags = load(data);
+    if (
+      tags !== undefined &&
+      tags.DateTimeOriginal !== undefined &&
+      tags.DateTimeOriginal.value !== undefined &&
+      tags.DateTimeOriginal.value[0] === undefined
+    ) {
+      return file.name;
+    }
+    const shootingDate = parseExifDate(tags.DateTimeOriginal.value[0]);
+    if (!(shootingDate instanceof Date)) {
+      return file.name;
+    }
+    const year = shootingDate.getFullYear();
+    const month = zeroPad(shootingDate.getMonth() + 1);
+    const day = zeroPad(shootingDate.getDate());
+    const hours = zeroPad(shootingDate.getHours());
+    const minutes = zeroPad(shootingDate.getMinutes());
+    const seconds = zeroPad(shootingDate.getSeconds());
+    const formattedDate = `${year}${month}${day}_${hours}${minutes}${seconds}`;
+    const index = file.name.lastIndexOf(".");
+    return formattedDate + file.name.substr(index);
+  } catch (error) {
     return file.name;
   }
-  const shootingDate = parseExifDate(tags.DateTimeOriginal.value[0]);
-  if (!(shootingDate instanceof Date)) {
-    return file.name;
-  }
-  const year = shootingDate.getFullYear();
-  const month = zeroPad(shootingDate.getMonth() + 1);
-  const day = zeroPad(shootingDate.getDate());
-  const hours = zeroPad(shootingDate.getHours());
-  const minutes = zeroPad(shootingDate.getMinutes());
-  const seconds = zeroPad(shootingDate.getSeconds());
-  const formattedDate = `${year}${month}${day}_${hours}${minutes}${seconds}`;
-  const index = file.name.lastIndexOf(".");
-  return formattedDate + file.name.substr(index);
 }
